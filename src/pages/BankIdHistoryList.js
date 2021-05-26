fec831f24f96f723084180e709967281387f5d8d
machntek <machntek@gmail.com>
2021-05-26 13:26:30 +0900

    feat: vc이력조회, fld조회 axios http통신 완성

--- src/pages/BankIdHistoryList.js
+++ src/pages/BankIdHistoryList.js
@@ -2,6 +2,7 @@ import { Helmet } from 'react-helmet';
 import { Box, Container, Typography } from '@material-ui/core';
 import Stack from '@material-ui/core/Stack';
 import { useState } from 'react';
+import axios from 'axios';
 import VcUsingHistoryResults from 'src/components/history/bankid/VcUsingHistoryResults';
 import DidSearchToolbar from 'src/components/history/bankid/DidSearchToolbar';
 import DidList from '../components/history/bankid/DidList';
@@ -18,78 +19,37 @@ const BankIdHistoryList = () => {
   const [vcHistory, setVcHistory] = useState({
     content: []
   });
-  const handleDidSearch = (cusno) => {
+  const searchDidByCusNo = (cusno) => {
     setVcs({ ...dids, content: [] });
     setVcHistory({ ...vcHistory, content: [] });
-    setDids({
-      ...dids,
-      content: [
-        {
-          did: 'did:omn:'.concat(cusno.value),
-          status: 1
-        },
-        {
-          did: 'did:omn:2sf',
-          status: 1
-        }
-      ]
-    });
-  };
-  const searchDidByCusNo = (cusno) => {
-    handleDidSearch(cusno);
-  };
-  const handleVcSearch = () => {
-    setVcs({
-      ...vcs,
-      content: [
-        {
-          vcId: '12vcid34test',
-          vcType: 'bankid',
-          status: 1
-        },
-        {
-          vcId: 'cccc',
-          vcType: 'bankid',
-          status: 1
-        }
-      ]
-    });
+
+    axios.get('http://localhost:8081/did/admin/myinfo/query/did', {
+      params: {
+        cusno: cusno.value
+      }
+    })
+      .then((Response) => {
+        setDids({ ...dids, content: Response.data.content });
+      }).catch((Error) => {
+        console.log(Error);
+      });
   };
   const searchVcByDid = () => {
-    handleVcSearch();
-  };
-
-  const handleVcHistorySearcy = () => {
-    setVcHistory({
-      ...vcHistory,
-      content: [
-        {
-          did: 'did:omn:1234',
-          vcId: '12vcid34test',
-          vcType: 1,
-          vcUseGb: 3,
-          datetime: '2021/05/05 05:05:05'
-        },
-        {
-          did: 'did:omn:1234',
-          vcId: '12vcid34test',
-          vcType: 1,
-          vcUseGb: 2,
-          datetime: '2021/05/06 05:05:05'
-        },
-        {
-          did: 'did:omn:1234',
-          vcId: '12vcid34test',
-          vcType: 1,
-          vcUseGb: 1,
-          datetime: '2021/05/07 05:05:05'
-        }
-      ]
-    });
+    axios.get('http://localhost:8081/did/admin/myinfo/query/vc')
+      .then((Response) => {
+        setVcs({ ...vcs, content: Response.data.content });
+      }).catch((Error) => {
+        console.log(Error);
+      });
   };
 
   const searchVcHistoryByVcId = () => {
-    handleVcHistorySearcy();
+    axios.get('http://localhost:8081/did/admin/myinfo/query/vc/history')
+      .then((Response) => {
+        setVcHistory({ ...vcHistory, content: Response.data.content });
+      }).catch((Error) => {
+        console.log(Error);
+      });
   };
 
   return (
--- src/pages/FldHistoryList.js
+++ src/pages/FldHistoryList.js
@@ -1,6 +1,7 @@
 import { Helmet } from 'react-helmet';
 import { Box, Container, Typography } from '@material-ui/core';
 import { useState } from 'react';
+import axios from 'axios';
 import FldHistoryListToolbar from '../components/history/fld/FldHistoryListToolbar';
 import FldHistoryResults from '../components/history/fld/FldHistoryResults';
 
@@ -9,28 +10,14 @@ const FldHistoryList = () => {
     content: []
   });
 
-  const handleFldHistorySearch = () => {
-    setHistories({
-      ...histories,
-      content: [
-        {
-          serialNo: 1000,
-          datetime: '2021/05/05 05:05:05',
-          kftcTrxNo: 1000,
-          sendDirection: '은행->금결원 요청'
-        },
-        {
-          serialNo: 2000,
-          datetime: '2021/05/05 05:05:05',
-          kftcTrxNo: 2000,
-          sendDirection: '은행->금결원 응답'
-        }
-      ]
-    });
-  };
-
   const searchFldHistories = () => {
-    handleFldHistorySearch();
+    axios.get('http://localhost:8081/did/admin/myinfo/query/fld')
+      .then((Response) => {
+        console.log(Response);
+        setHistories({ ...histories, content: Response.data.content });
+      }).catch((Error) => {
+        console.log(Error);
+      });
   };
 
   return (
